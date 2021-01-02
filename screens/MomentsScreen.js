b1c7f8640ff209563965d5eedd9b160d14eb78d7
Chengyu Zhou <zhouchengyu95@gmail.com>
2021-01-02 11:05:02 +1000

    Changed MomentsScreen to a function

--- screens/MomentsScreen.js
+++ screens/MomentsScreen.js
@@ -1,82 +1,22 @@
-import React, { Component } from "react";
+import React from "react";
 import { StyleSheet, Text, View, ImageBackground, Alert } from "react-native";
-import { createAppContainer } from "react-navigation";
-import { createMaterialTopTabNavigator } from 'react-navigation-tabs';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 import { RFValue } from "react-native-responsive-fontsize";
 
 import { getBackgroundImage } from "../components/images";
 import Header from "../components/Header";
 
-function completeTips(rewardHealth) {
-  baseHealth += rewardHealth["avatarHealth"];
-  baseStatus += rewardHealth["avatarStatus"];
-  baseCurrency += rewardHealth["shopCurrency"];
-  this.setState((state, props) => ({
-    baseHealth: this.state.baseHealth + rewardHealth["avatarHealth"],
-    baseCurr: this.state.baseCurr + rewardHealth["shopCurrency"],
-    baseStatus: this.state.baseStatus + rewardHealth["avatarStatus"],
-  }));
-}
-
-// Reading Alert to verify if the tip is read.
-function reading(item) {
-  Alert.alert(
-    item["tipName"],
-    item["tip"],
-    [
-      {
-        text: "Cancel",
-        onPress: () => console.log("cancel"),
-        style: "cancel",
-      },
-      {
-        text: "OK",
-        onPress: () => {
-          completeTips(item["tipReward"]);
-          submitTip(
-            item["tipID"],
-            item["tipName"],
-            item["tip"],
-            item["tipType"],
-            item["tipReward"],
-            !item["complete"]
-          );
-        },
-      },
-    ],
-    { cancelable: false }
+const MomentsScreen = ({ navigation }) => {
+  const background = getBackgroundImage(2);
+  return (
+    <View>
+      <ImageBackground source={background} style={styles.backgroundImage}>
+        <Header navigation={navigation} />
+        <View style={styles.educationalWebsites}>
+        </View>
+      </ImageBackground>
+    </View>
   );
-}
-
-export default class MomentsScreen extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      baseHealth: 0,
-      baseStatus: 0,
-      baseCurr: 0,
-      backgroundColor: 6,
-    };
-  }
-
-  handleCurrency = () => {
-    return <Text style={{ fontSize: 25, color: "white" }}>{baseCurrency}</Text>;
-  };
-
-  render() {
-    const background = getBackgroundImage(this.state.backgroundColor);
-    return (
-      <View>
-        <ImageBackground source={background} style={styles.backgroundImage}>
-          <Header route={this.props} />
-          <View style={styles.educationalWebsites}>
-
-          </View>
-        </ImageBackground>
-      </View>
-    );
-  }
 }
 
 
@@ -92,3 +32,5 @@ const styles = StyleSheet.create({
     flex: 1
   },
 })
+
+export default MomentsScreen
